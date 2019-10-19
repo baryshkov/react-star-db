3fa0036bbe5295788c5135c9a70a0f9788ee13db
baryshkov <ambaryshkov@gmail.com>
2019-10-19 23:30:43 +0300

    added swapi service

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
+import 'flexboxgrid2/flexboxgrid2.css';
+import './index.css';
+import App from './App';
 
+ReactDOM.render(<App />, document.getElementById('root'));
